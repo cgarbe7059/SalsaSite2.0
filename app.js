9ab710bf406eb8690726008971b34440195d6ae8
Candiss Garber <candissg22@gmail.com>
2022-08-02 22:55:50 -0400

    final fixes

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 function test (button) {
 	var val = parseFloat(button.getAttribute("value"));
-	var kwan = parseFloat(document.getElementById('quantity').value);
     var total = parseFloat(document.getElementById('totaltext').getAttribute("value"));
 	var items = parseFloat(document.getElementById('items').getAttribute("value"));
-	
-	if(button.getAttribute("name") === 'product')
 
 
-	document.getElementById('totaltext').innerHTML = total + val * kwan;
-    document.getElementById('totaltext').setAttribute("value", total + val * kwan);
+	document.getElementById('totaltext').innerHTML = total + val;
+    document.getElementById('totaltext').setAttribute("value", total + val);
 	
 	document.getElementById('items').innerHTML = items + kwan;
 	document.getElementById('items').setAttribute("value", items + kwan);
@@ -16,4 +13,11 @@ function test (button) {
 	
 {"}"} }
 
+var clicks = 0;
 
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+};
+<button type="button" onClick="onClick()">Click me</button>
+<p>Clicks: <a id="clicks">0</a></p>
